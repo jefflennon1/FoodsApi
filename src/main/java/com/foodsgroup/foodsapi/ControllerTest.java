5da5b39e9da2810edc1956eeda9d11b38cec5077
jefflenno1 <jeffersonlennon1@outlook.com>
2023-07-26 19:12:12 -0300

    testing injection of dependecys types

--- src/main/java/com/foodsgroup/foodsapi/ControllerTest.java
+++ src/main/java/com/foodsgroup/foodsapi/ControllerTest.java
@@ -1,13 +1,24 @@
 package com.foodsgroup.foodsapi;
 
+import com.foodsgroup.foodsapi.model.ClientModel;
+import com.foodsgroup.foodsapi.notification.Notificator;
+import com.foodsgroup.foodsapi.service.ActivateClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
 
 @RestController
 public class ControllerTest {
 
+    @Inject
+    private ActivateClientService activateClientService;
+
     @GetMapping("/hello")
     public String hello(){
+        ClientModel client = new ClientModel("Jefferson", "jefferson@xyz", "85999999");
+        activateClientService.activate(client);
         return "Hello";
     }
 }
--- src/main/java/com/foodsgroup/foodsapi/service/ActivateClientService.java
+++ src/main/java/com/foodsgroup/foodsapi/service/ActivateClientService.java
@@ -1,13 +1,17 @@
 package com.foodsgroup.foodsapi.service;
 
+import com.fasterxml.jackson.annotation.JacksonInject;
 import com.foodsgroup.foodsapi.model.ClientModel;
 import com.foodsgroup.foodsapi.notification.EmailNotification;
 import com.foodsgroup.foodsapi.notification.Notificator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ActivateClientService {
 
+
+    @Autowired(required = false)
     private Notificator notificator;
 
     public ActivateClientService(EmailNotification notificator) {

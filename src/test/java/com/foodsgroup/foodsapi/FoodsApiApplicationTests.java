6a191209cb3d5bfe2730ea1eb21dfd256a6d53bf
jefflenno1 <jeffersonlennon1@outlook.com>
2023-07-26 11:28:52 -0300

    incial commit

---
+++ src/test/java/com/foodsgroup/foodsapi/FoodsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodsgroup.foodsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
